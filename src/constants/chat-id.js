247243d6c76fad2c6540c505f9d06ec1a057c7eb
Viktor Honcharuk <viktor.honcharuk.gepur@gmail.com>
2021-01-11 17:05:16 +0200

    Updated chatIds

--- src/constants/chat-id.js
+++ src/constants/chat-id.js
@@ -1,7 +1,10 @@
 export const chatIds = [
-    { 
+    {
         id: 1034611868,
         name: 'Viktor Hocnharuk',
     },
+    {
+        id: 568548214,
+        name: 'Natalya Barday',
+    }
 ]
-
