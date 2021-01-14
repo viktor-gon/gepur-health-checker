78b4e32173595c92d3f7164c9074d9b59a117936
Viktor Honcharuk <viktor.honcharuk.gepur@gmail.com>
2021-01-14 16:54:58 +0200

    Added status command

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-import { checkUrl } from './helpers/index';
-import { chatIds as chatIdsDefault } from './constants/chat-id';
-import { urlsToCheck, Target } from './constants/urls';
-import { saveDataJSON, getDataJSON } from './helpers/json-fs';
+import {checkUrl} from './helpers/index';
+import {chatIds as chatIdsDefault} from './constants/chat-id';
+import {urlsToCheck, Target} from './constants/urls';
+import {saveDataJSON, getDataJSON} from './helpers/json-fs';
 
 const chatIds = getDataJSON() || chatIdsDefault;
 
@@ -17,94 +17,98 @@ const token = process.env.BOT_TOKEN_ID;
 const startPin = process.env.START_PIN;
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, {polling: true});
 
 const sendMessageToAllChats = (message) => {
-  chatIds.forEach((chat) => {
-    if (chat.id) {
-      bot.sendMessage(chat.id, message);
-    }
-  });
+    chatIds.forEach((chat) => {
+        if (chat.id) {
+            bot.sendMessage(chat.id, message);
+        }
+    });
 };
 
 const checkGivenUrls = async (host, urlsToCheck, mobile) => {
-  const agent = mobile ? 'mobile' : 'desktop';
+    const agent = mobile ? 'mobile' : 'desktop';
 
-  const options = {
-    headers: {
-      'user-agent':
-        agent === 'mobile' ? 'gepur-telegram-mobile' : 'gepur-telegram',
-    },
-  };
+    const options = {
+        headers: {
+            'user-agent':
+                agent === 'mobile' ? 'gepur-telegram-mobile' : 'gepur-telegram',
+        },
+    };
 
-  urlsToCheck.forEach(async (url) => {
-    const targetUrl = `https://${host}${url.link}`;
+    urlsToCheck.forEach(async (url) => {
+        const targetUrl = `https://${host}${url.link}`;
 
-    if (agent === 'mobile' && !url.target.includes(Target.MOBILE)) {
-      return;
-    }
+        if (agent === 'mobile' && !url.target.includes(Target.MOBILE)) {
+            return;
+        }
 
-    if (agent === 'desktop' && !url.target.includes(Target.DESKTOP)) {
-      return;
-    }
+        if (agent === 'desktop' && !url.target.includes(Target.DESKTOP)) {
+            return;
+        }
 
-    try {
-      const response = await checkUrl(targetUrl, options);
-      console.log(`[${agent}]`, targetUrl, response.status);
+        try {
+            const response = await checkUrl(targetUrl, options);
+            console.log(`[${agent}]`, targetUrl, response.status);
 
-      if (response.status >= 400) {
-        console.error('error:', response.status, targetUrl, agent);
-        sendMessageToAllChats(`${targetUrl} : ${response.status} ${agent}`);
-      }
-    } catch (err) {
-      console.error('error:', targetUrl, agent);
-      sendMessageToAllChats(`${targetUrl} ${err} ${agent}`);
-    }
-  });
+            if (response.status >= 400) {
+                console.error('error:', response.status, targetUrl, agent);
+                sendMessageToAllChats(`${targetUrl} : ${response.status} ${agent}`);
+            }
+        } catch (err) {
+            console.error('error:', targetUrl, agent);
+            sendMessageToAllChats(`${targetUrl} ${err} ${agent}`);
+        }
+    });
 };
 
 const findChat = (id) => chatIds.find((element) => element.id === id);
 
 const checkSite = async (testDomain) => {
-  console.log('check...');
-  console.log(chatIds);
+    console.log('check...');
+    console.log(chatIds);
 
-  // desktop
-  checkGivenUrls(testDomain, urlsToCheck, false);
+    // desktop
+    checkGivenUrls(testDomain, urlsToCheck, false);
 
-  // mobile
-  checkGivenUrls(testDomain, urlsToCheck, true);
+    // mobile
+    checkGivenUrls(testDomain, urlsToCheck, true);
 
-  setTimeout(() => checkSite(testDomain), 60000 * minutesInterval);
+    setTimeout(() => checkSite(testDomain), 60000 * minutesInterval);
 };
 
 bot.onText(/\/start (.+)/, (msg, match) => {
-  const resp = match[1]; // the captured "whatever"
-  const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+    const chatId = msg.chat.id;
 
-  if (resp === startPin) {
-    bot.sendMessage(msg.chat.id, `Начинаем следить за сайтом ${testDomain}`);
-    bot.sendMessage(msg.chat.id, msg.chat.id);
-    if (!findChat(chatId)) {
-      const name = `${msg.from.first_name} ${msg.from.last_name}`;
-      console.log(name);
-      chatIds.push({ id: chatId, name });
-      saveDataJSON(chatIds);
+    if (resp === startPin) {
+        bot.sendMessage(msg.chat.id, `Начинаем следить за сайтом ${testDomain}`);
+        bot.sendMessage(msg.chat.id, msg.chat.id);
+        if (!findChat(chatId)) {
+            const name = `${msg.from.first_name} ${msg.from.last_name}`;
+            console.log(name);
+            chatIds.push({id: chatId, name});
+            saveDataJSON(chatIds);
+        }
     }
-  }
+});
+
+bot.onText(/\/status/, (msg, match) => {
+    bot.sendMessage(msg.chat.id, `Работаем ${testDomain}`);
 });
 
 bot.onText(/\/stop/, (msg, match) => {
-  const resp = match[1]; // the captured "whatever"
+    const resp = match[1]; // the captured "whatever"
 
-  const chat = findChat(msg.chat.id);
+    const chat = findChat(msg.chat.id);
 
-  if (chat) {
-    chat.id = 0;
-    saveDataJSON(chatIds.filter(chat => chat.id));
-  }
+    if (chat) {
+        chat.id = 0;
+        saveDataJSON(chatIds.filter(chat => chat.id));
+    }
 
-  bot.sendMessage(msg.chat.id, `Перестаем следить за сайтом ${testDomain}`);
+    bot.sendMessage(msg.chat.id, `Перестаем следить за сайтом ${testDomain}`);
 });
 
 checkSite(testDomain);
